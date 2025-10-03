9b55b2e35f16ff69ca70a38cb82b4a86502a21ee
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2025-10-03 11:55:07 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const http = require('http');
 const express = require('express');
-// const { Server } = require('socket.io'); // <-- comenta pra testar
+const { Server } = require('socket.io'); // <-- comenta pra testar
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => res.json({ ok: true }));
 const server = http.createServer(app);
 
 // // Socket.IO comentado no teste
-// const io = new Server(server, { path: '/socket.io' });
+const io = new Server(server, { path: '/socket.io' });
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`UP na porta ${PORT}`);
