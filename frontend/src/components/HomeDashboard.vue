384bf0680aacd50da1b94aa301c04d4b7892618d
Matheus Loch <122698448+Roonyx43@users.noreply.github.com>
2025-12-05 14:10:56 -0300

    Inclusão de Dashboard de Faturamento

--- frontend/src/components/HomeDashboard.vue
+++ frontend/src/components/HomeDashboard.vue
@@ -37,6 +37,14 @@
         <h3 class="text-2xl font-bold mb-3 text-red-400">Pedidos</h3>
         <p class="text-sm text-gray-300">Aguardando PCP</p>
       </RouterLink>
+
+      <RouterLink
+        to="/dashboards/faturamento"
+        class="flex flex-col items-center justify-center rounded-2xl border-2 border-red-500 p-8 shadow-lg hover:scale-105 transition transform bg-gray-800"
+      >
+        <h3 class="text-2xl font-bold mb-3 text-red-400">Faturamento</h3>
+        <p class="text-sm text-gray-300">Aprovados + Aguardando PCP + Denegados</p>
+      </RouterLink>
     </div>
   </section>
 </template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import DashboardGeral from '@/components/Dashboard/DashboardGeral.vue';
 import DashboardFin from '@/components/Dashboard/DashboardFinanceiro.vue';
 import DashboardPedidosFin from '@/components/Dashboard/DashboardComercial.vue';
 import DashboardAguardandoPCP from '../components/Dashboard/DashboardAguardandoPCP.vue';
+import DashboardFaturamento from '../components/Dashboard/DashboardFaturamento.vue';
 
 const routes = [
   { path: '/', name: 'home', component: HomeDashboards },
@@ -13,6 +14,7 @@ const routes = [
   { path: '/dashboards/fin', name: 'dashboard-fin', component: DashboardFin },
   { path: '/dashboards/pedidos-fin', name: 'dashboard-pedidos-fin', component: DashboardPedidosFin },
   { path: '/dashboards/aguardando-pcp', name: 'dashboard-aguardando-pcp', component: DashboardAguardandoPCP },
+  { path: '/dashboards/faturamento', name: 'dashboard-faturamento', component: DashboardFaturamento }
 ];
 
 export const router = createRouter({
